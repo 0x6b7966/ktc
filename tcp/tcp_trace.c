eb120a8aaf803873610564f19053c687c37534a6
Zwb <ethercflow@gmail.com>
2018-09-04 17:26:37 +0800

    feat(tcp_trace): trace retransmit_skb,and tcp_send_loss_probe

---
+++ tcp/tcp_trace.c
@@ -0,0 +1,63 @@
+#include <linux/module.h>
+#include <linux/kprobes.h>
+#include <net/tcp.h>
+
+#define CREATE_TRACE_POINTS
+#include "tcp_trace.h"
+
+static int jtcp_retransmit_skb(struct sock *sk, struct sk_buff *skb)
+{
+    trace_tcp_retransmit_skb(sk, skb);
+    jprobe_return();
+    return 0;
+}
+
+static struct jprobe tcp_retransmit_skb_jp = {
+    .kp = {
+        .symbol_name = "tcp_retransmit_skb",
+    },
+    .entry = jtcp_retransmit_skb,
+};
+
+static int jtcp_send_loss_probe(struct sock *sk)
+{
+    trace_tcp_send_loss_probe(sk);
+    jprobe_return();
+    return 0;
+}
+
+static struct jprobe tcp_send_loss_probe_jp = {
+    .kp = {
+        .symbol_name = "tcp_send_loss_probe",
+    },
+    .entry = jtcp_send_loss_probe,
+};
+
+static struct jprobe *tcp_jprobes[] = {
+    &tcp_retransmit_skb_jp,
+    &tcp_send_loss_probe_jp
+};
+
+static int __init tcp_trace_init(void) {
+    int ret;
+
+    ret = register_jprobes(tcp_jprobes, sizeof(tcp_jprobes) / sizeof(tcp_jprobes[0]));
+    if(ret) {
+        pr_err("Register tcp jprobes failed\n");
+        return ret;
+    }
+    pr_info("Register tcp jprobe successed\n");
+
+    return 0;
+}
+
+static void __exit tcp_trace_exit(void) {
+    unregister_jprobes(tcp_jprobes, sizeof(tcp_jprobes) / sizeof(tcp_jprobes[0]));
+    pr_info("unregister tcp jprobes successed\n");
+}
+
+MODULE_AUTHOR("Zwb <ethercflow@gmail.com>");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
+module_init(tcp_trace_init);
+module_exit(tcp_trace_exit);
---
+++ tcp/tcp_trace.h
@@ -0,0 +1,157 @@
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM tcp
+
+#if !defined(_TRACE_TCP_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_TCP_H
+
+#include <linux/ipv6.h>
+#include <linux/tcp.h>
+#include <linux/tracepoint.h>
+#include <net/ipv6.h>
+#include <net/tcp.h>
+#include <linux/sock_diag.h>
+
+#define TP_STORE_V4MAPPED(__entry, saddr, daddr)		\
+    do {							\
+        struct in6_addr *pin6;				\
+                                \
+        pin6 = (struct in6_addr *)__entry->saddr_v6;	\
+        ipv6_addr_set_v4mapped(saddr, pin6);		\
+        pin6 = (struct in6_addr *)__entry->daddr_v6;	\
+        ipv6_addr_set_v4mapped(daddr, pin6);		\
+    } while (0)
+
+#if IS_ENABLED(CONFIG_IPV6)
+#define TP_STORE_ADDRS(__entry, saddr, daddr, saddr6, daddr6)		\
+    do {								\
+        if (sk->sk_family == AF_INET6) {			\
+            struct in6_addr *pin6;				\
+                                    \
+            pin6 = (struct in6_addr *)__entry->saddr_v6;	\
+            *pin6 = saddr6;					\
+            pin6 = (struct in6_addr *)__entry->daddr_v6;	\
+            *pin6 = daddr6;					\
+        } else {						\
+            TP_STORE_V4MAPPED(__entry, saddr, daddr);	\
+        }							\
+    } while (0)
+#else
+#define TP_STORE_ADDRS(__entry, saddr, daddr, saddr6, daddr6)	\
+    TP_STORE_V4MAPPED(__entry, saddr, daddr)
+#endif
+
+/*
+ * tcp event with arguments sk and skb
+ *
+ * Note: this class requires a valid sk pointer; while skb pointer could
+ *       be NULL.
+ */
+DECLARE_EVENT_CLASS(tcp_event_sk_skb,
+
+    TP_PROTO(const struct sock *sk, const struct sk_buff *skb),
+
+    TP_ARGS(sk, skb),
+
+    TP_STRUCT__entry(
+        __field(const void *, skbaddr)
+        __field(const void *, skaddr)
+        __field(__u16, sport)
+        __field(__u16, dport)
+        __array(__u8, saddr, 4)
+        __array(__u8, daddr, 4)
+        __array(__u8, saddr_v6, 16)
+        __array(__u8, daddr_v6, 16)
+    ),
+
+    TP_fast_assign(
+        struct inet_sock *inet = inet_sk(sk);
+        __be32 *p32;
+
+        __entry->skbaddr = skb;
+        __entry->skaddr = sk;
+
+        __entry->sport = ntohs(inet->inet_sport);
+        __entry->dport = ntohs(inet->inet_dport);
+
+        p32 = (__be32 *) __entry->saddr;
+        *p32 = inet->inet_saddr;
+
+        p32 = (__be32 *) __entry->daddr;
+        *p32 =  inet->inet_daddr;
+
+        TP_STORE_ADDRS(__entry, inet->inet_saddr, inet->inet_daddr,
+                  sk->sk_v6_rcv_saddr, sk->sk_v6_daddr);
+    ),
+
+    TP_printk("sport=%hu dport=%hu saddr=%pI4 daddr=%pI4 saddrv6=%pI6c daddrv6=%pI6c",
+          __entry->sport, __entry->dport, __entry->saddr, __entry->daddr,
+          __entry->saddr_v6, __entry->daddr_v6)
+);
+
+DEFINE_EVENT(tcp_event_sk_skb, tcp_retransmit_skb,
+
+    TP_PROTO(const struct sock *sk, const struct sk_buff *skb),
+
+    TP_ARGS(sk, skb)
+);
+
+/*
+ * tcp event with arguments sk
+ *
+ * Note: this class requires a valid sk pointer.
+ */
+DECLARE_EVENT_CLASS(tcp_event_sk,
+
+    TP_PROTO(struct sock *sk),
+
+    TP_ARGS(sk),
+
+    TP_STRUCT__entry(
+        __field(const void *, skaddr)
+        __field(__u16, sport)
+        __field(__u16, dport)
+        __array(__u8, saddr, 4)
+        __array(__u8, daddr, 4)
+        __array(__u8, saddr_v6, 16)
+        __array(__u8, daddr_v6, 16)
+    ),
+
+    TP_fast_assign(
+        struct inet_sock *inet = inet_sk(sk);
+        __be32 *p32;
+
+        __entry->skaddr = sk;
+
+        __entry->sport = ntohs(inet->inet_sport);
+        __entry->dport = ntohs(inet->inet_dport);
+
+        p32 = (__be32 *) __entry->saddr;
+        *p32 = inet->inet_saddr;
+
+        p32 = (__be32 *) __entry->daddr;
+        *p32 =  inet->inet_daddr;
+
+        TP_STORE_ADDRS(__entry, inet->inet_saddr, inet->inet_daddr,
+                   sk->sk_v6_rcv_saddr, sk->sk_v6_daddr);
+    ),
+
+    TP_printk("sport=%hu dport=%hu saddr=%pI4 daddr=%pI4 saddrv6=%pI6c daddrv6=%pI6c",
+          __entry->sport, __entry->dport,
+          __entry->saddr, __entry->daddr,
+          __entry->saddr_v6, __entry->daddr_v6)
+);
+
+DEFINE_EVENT(tcp_event_sk, tcp_send_loss_probe,
+
+    TP_PROTO(struct sock *sk),
+
+    TP_ARGS(sk)
+);
+
+#endif /* _TRACE_TCP_H */
+
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH .
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_FILE tcp_trace
+#include <trace/define_trace.h>
