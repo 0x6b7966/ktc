8002612d95a4769459a6ba13fdc7a04aacaed4ce
Zwb <ethercflow@gmail.com>
2018-10-15 11:58:14 +0800

    feat(tcp_drop): init netlink

--- src/modules/lp_tcp/lp_tcp.c
+++ src/modules/lp_tcp/lp_tcp.c
@@ -1,5 +1,8 @@
 #include <linux/module.h>
 #include <linux/livepatch.h>
+#include <linux/netlink.h>
+#include <linux/percpu.h>
+#include <net/genetlink.h>
 
 #include "sock.c"
 #include "fib_frontend.c"
@@ -10,6 +13,26 @@
 #include "icmp.c"
 #include "route.c"
 #include "tcp_ipv4.c"
+
+struct per_cpu_dm_data {
+    spinlock_t		lock;
+    struct sk_buff		*skb;
+};
+
+static struct genl_family net_drop_monitor_family;
+
+static DEFINE_PER_CPU(struct per_cpu_dm_data, dm_cpu_data);
+
+static const struct genl_ops dropmon_ops[] = {};
+
+static struct genl_family net_drop_monitor_family = {
+    .hdrsize        = 0,
+    .name           = "NET_DM",
+    .version        = 2,
+    .module		= THIS_MODULE,
+    .ops		= dropmon_ops,
+    .n_ops		= ARRAY_SIZE(dropmon_ops),
+};
 
 static struct klp_func funcs[] = {
     {
@@ -29,20 +52,66 @@ static struct klp_patch patch = {
     .objs = objs,
 };
 
+static int init_net_drop_monitor(void)
+{
+    struct per_cpu_dm_data *data;
+    int cpu, rc;
+
+    pr_info("Initializing network drop monitor service\n");
+
+    rc = genl_register_family(&net_drop_monitor_family);
+    if (rc) {
+        pr_err("Could not create drop monitor netlink family\n");
+        return rc;
+    }
+
+    rc = 0;
+
+    for_each_possible_cpu(cpu) {
+        data = &per_cpu(dm_cpu_data, cpu);
+        spin_lock_init(&data->lock);
+    }
+
+    goto out;
+
+out_unreg:
+    genl_unregister_family(&net_drop_monitor_family);
+out:
+    return rc;
+}
+
+static void exit_net_drop_monitor(void)
+{
+    struct per_cpu_dm_data *data;
+    int cpu;
+
+    for_each_possible_cpu(cpu) {
+        data = &per_cpu(dm_cpu_data, cpu);
+        kfree_skb(data->skb);
+    }
+
+    BUG_ON(genl_unregister_family(&net_drop_monitor_family));
+}
+
 static int __init lp_tcp_init(void)
 {
-    int ret;
+    int rc;
+
+    rc = init_net_drop_monitor();
+    if (rc)
+        return rc;
 
     if (!klp_have_reliable_stack() && !patch.immediate) {
         patch.immediate = true;
-        pr_notice("The consistency model isn't supported for your architecture.  Bypassing safety mechanisms and applying the patch immediately.\n");
+        pr_notice("The consistency model isn't supported for your architecture. "
+                  "Bypassing safety mechanisms and applying the patch immediately.\n");
     }
 
-    ret = klp_register_patch(&patch);
-    if (ret)
-        return ret;
-    ret = klp_enable_patch(&patch);
-    if (ret) {
+    rc = klp_register_patch(&patch);
+    if (rc)
+        return rc;
+    rc = klp_enable_patch(&patch);
+    if (rc) {
         WARN_ON(klp_unregister_patch(&patch));
         return ret;
     }
@@ -52,6 +121,7 @@ static int __init lp_tcp_init(void)
 
 static void __exit lp_tcp_exit(void)
 {
+    exit_net_drop_monitor();
     WARN_ON(klp_unregister_patch(&patch));
 }
 
