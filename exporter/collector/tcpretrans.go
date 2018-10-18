ef9935718bf5fb234cece9c221b60839bc139009
Zwb <ethercflow@gmail.com>
2018-10-18 15:05:59 +0800

    feat(exporter): impl tcpretrans info collector

--- exporter/collector/tcpretrans.go
+++ exporter/collector/tcpretrans.go
@@ -1,29 +1,94 @@
 package collector
 
 import (
-	"fmt"
+	"bufio"
+	"os/exec"
+	"regexp"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
         _ "github.com/prometheus/common/log"
         _ "gopkg.in/alecthomas/kingpin.v2"
 )
 
+const (
+	script = "./tcpretrans"
+)
+
+var (
+	scanner *bufio.Scanner
+	ready = make(chan bool)
+	info = make(chan string, 4096)
+)
+
 type tcpretransCollector struct {
-	
+	retrans typedDesc 
 }
 
-
 func init() {
-	registerCollector("tcpretrans", defaultEnabled, NewTCPRetransCollector)	
+	runTCPRetransScript()
+	parseTCPRetransInfo()
+	registerCollector("tcpretrans", defaultEnabled, NewTCPRetransCollector)
+}
+
+func runTCPRetransScript() {
+	go func() {
+		cmd := exec.Command(script)
+		cmdReader, _ := cmd.StdoutPipe()
+		scanner = bufio.NewScanner(cmdReader)
+		cmd.Start()
+		ready <- true
+		cmd.Wait()
+	}()
+}
+
+func parseTCPRetransInfo() {
+	go func() {
+		re_inside_whtsp := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
+		waitPipeReady()
+		if scanner.Scan() {
+			scanner.Text()
+		}
+		for scanner.Scan() {
+			line := strings.SplitN(
+				re_inside_whtsp.ReplaceAllString(scanner.Text(), " "), " ", 4)
+			info <- line[len(line) - 1]
+		}
+	}()
+}
+
+func waitPipeReady() {
+	<- ready
+}
+
+func getTCPRetransInfo() []string {
+	items := []string{}
+	for {
+		select {
+			case i := <- info:
+				items = append(items, i)
+			default:
+				goto end
+		}
+	}
+end:
+	return items
 }
 
 func NewTCPRetransCollector() (Collector, error) {
-	fmt.Println("In NewTCPRetransCollector")	
-	return &tcpretransCollector{}, nil
+	return &tcpretransCollector{
+		retrans: typedDesc{prometheus.NewDesc(
+                        prometheus.BuildFQName(namespace, "tcp", "retrans"),
+                        "Tcp retrans info.",
+                        []string{"tcp"}, nil,
+                ), prometheus.GaugeValue},
+	}, nil
 }
 
 func (c *tcpretransCollector) Update(ch chan <- prometheus.Metric) error {
-	fmt.Println("tcpretransCollector.Update be called")	
-	ch <- nil	
+	items := getTCPRetransInfo()
+	for _, item := range items {
+		ch <- c.retrans.mustNewConstMetric(1, item)
+	}
 	return nil
 }
